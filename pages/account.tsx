6e73e80822d2c330232b6a6d12fd909ab275d676
Fabio Viscuso <viscuso.fabio@outlook.it>
2022-10-02 18:52:03 +0200

    feat: misc fixes and added initial password change support

--- pages/account.tsx
+++ pages/account.tsx
@@ -3,7 +3,8 @@ import React, { useEffect, useRef } from "react";
 import useStore from "../store/Store";
 
 const Account = () => {
-    const emailInputRef = useRef<HTMLInputElement>(null)
+    const emailInputRef = useRef<HTMLInputElement>(null);
+    const passInputRef = useRef<HTMLInputElement>(null);
     const isLogged = useStore(state => state.isLogged);
     const user = useStore(state => state.currentUser);
     const login = useStore(state => state.loginUser)
@@ -31,13 +32,36 @@ const Account = () => {
                 console.log(data)
                 login(data.username, data.email, data.accessToken)
                 localStorage.setItem('currentUser', JSON.stringify({ username: data.username, email: data.email, accessoToken: data.accessToken }))
-                emailInputValue = ''
+                emailInputRef.current!.value = ''
             } else {
                 const data = await response.json()
                 alert(data.message)
             }
         } else {
             alert('check the validity of email input')
+        }
+    }
+
+    async function changePassword(event: React.FormEvent) {
+        event.preventDefault()
+        let passInputValue = passInputRef.current!.value
+        if (passInputValue.length > 7) {
+            const response = await fetch('api/users/changepsw', {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ username: user.username, newPassword: passInputValue, accessToken: user.accessToken })
+            })
+
+            if (response.ok) {
+                passInputRef.current!.value = ''
+            } else {
+                const data = await response.json()
+                alert(data.message)
+            }
+        } else {
+            alert('check the validity of password input')
         }
     }
 
@@ -48,7 +72,7 @@ const Account = () => {
     }, [isLogged])
 
     return (
-        <div className="flex flex-col min-h-screen items-center px-2 py-4 pt-8">
+        <div className="flex flex-col min-h-screen max-w-4xl px-2 mx-auto py-4 pt-8">
             <h1 className="font-caveat text-6xl mb-10">Hello, {user.username}!</h1>
             <form onSubmit={changeMail} className="flex flex-col gap-5 items-center mb-8">
                 <label htmlFor="changemail" className="text-gray-800 text-2xl font-indieflower leading-tight tracking-normal">Change email</label>
@@ -56,15 +80,15 @@ const Account = () => {
                 <input type='email' id="changemail" name="changemail" ref={emailInputRef} minLength={8} className="text-gray-600 focus:outline-none focus:border focus:border-pink-300 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" placeholder="New email Here" />
                 <button type="submit" className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-300 transition duration-150 ease-in-out hover:bg-pink-300 bg-pink-200 rounded text-gray-900 hover:text-gray-900 px-8 py-2 text-md">Submit</button>
             </form>
-            <form className="flex flex-col gap-5 items-center mb-8">
-                <label htmlFor="changepassword" className="text-gray-800 text-xl font-indieflower leading-tight tracking-normal">Change password</label>
-                <input type='password' id="changepassword" name="changepassword" minLength={8} className="text-gray-600 focus:outline-none focus:border focus:border-pink-300 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" placeholder="New Password Here" />
+            <form onSubmit={changePassword} className="flex flex-col gap-5 items-center mb-8">
+                <label htmlFor="changepassword" className="text-gray-800 text-2xl font-indieflower leading-tight tracking-normal">Change password</label>
+                <input type='password' id="changepassword" name="changepassword" ref={passInputRef} minLength={8} className="text-gray-600 focus:outline-none focus:border focus:border-pink-300 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" placeholder="New Password Here" />
                 <button type="submit" className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-300 transition duration-150 ease-in-out hover:bg-pink-300 bg-pink-200 rounded text-gray-900 hover:text-gray-900 px-8 py-2 text-md">Submit</button>
             </form>
             <h2 className="font-caveat text-4xl mt-10 mb-4">Other settings</h2>
-            <button onClick={clearCart} className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-300 transition duration-150 ease-in-out hover:bg-pink-300 bg-pink-200 rounded text-gray-900 hover:text-gray-900 px-8 py-2 mb-4 text-md">Clear Cart</button>
+            <button onClick={clearCart} className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-300 transition duration-150 ease-in-out hover:bg-pink-300 bg-pink-200 rounded text-gray-900 hover:text-gray-900 px-8 py-2 mb-10 text-md">Clear Cart</button>
             <form className="flex flex-col gap-5 items-center">
-                <label htmlFor="deleteaccount" className="text-gray-800 text-xl font-indieflower leading-tight tracking-normal">Delete your account</label>
+                <label htmlFor="deleteaccount" className="text-gray-800 text-2xl font-indieflower leading-tight tracking-normal">Delete your account</label>
                 <input type='password' id="deleteaccount" name="deleteaccount" minLength={8} className="text-gray-600 focus:outline-none focus:border focus:border-pink-300 font-normal w-full h-10 flex items-center pl-3 text-sm border-gray-300 rounded border" placeholder="Retype your username" />
                 <button type="submit" className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-300 transition duration-150 ease-in-out hover:bg-pink-300 bg-pink-200 rounded text-gray-900 hover:text-gray-900 px-8 py-2 text-md">Delete</button>
             </form>
--- pages/api/users/signup.ts
+++ pages/api/users/signup.ts
@@ -4,17 +4,15 @@ import env from '../../../lib/config';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
-        await env.prisma.users.create({
+        env.prisma.users.create({
             data: {
                 username: req.body.username,
                 email: req.body.email,
-                password: bcrypt.hashSync(req.body.password, 8)
+                password: bcrypt.hashSync(req.body.password, 10)
             }
         })
             .then(() => {
-                if (req.body.roles) {
-                    res.json({ message: "User registered successfully!" });
-                }
+                res.status(201).json({ message: "User registered successfully!" });
             }
             )
             .catch((err: any) => {
